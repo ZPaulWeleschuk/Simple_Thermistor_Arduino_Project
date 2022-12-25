72573e4bf2e8212378f569bd4421da4d928e90d3
Z. Paul Weleschuk <59177705+ZPaulWeleschuk@users.noreply.github.com>
2022-12-25 15:23:52 -0700

    definitions for 7 seg display
    definitions for seven segment display

---
+++ simple_thermistor_project.ino/simple_thermistor_project.ino.ino
@@ -0,0 +1,47 @@
+/*
+ *     ┌───┐
+ *         A  
+ * ┌ ┐└───┘┌ ┐
+ * │ │        │ │
+ * │F│        │B│
+ * │ │        │ │
+ * └ ┘┌───┐└ ┘
+ *         G
+ * ┌ ┐└───┘┌ ┐
+ * │ │        │ │
+ * │F│        │B│
+ * │ │        │ │
+ * └ ┘┌───┐└ ┘┌─┐
+ *         D         DP
+ *     └───┘    └─┘
+ *                 
+ * 
+ * 
+ */
+
+#define segA
+#define segB
+#define segC
+#define segD
+#define segE
+#define segF
+#define segG
+#define segDP
+
+//note: digits start on left; 2031 == {dig1 = 2, dig2 = 0, dig3 =3, dig4 = 1}
+#define dig1
+#define dig2
+#define dig3
+#define dig4
+#define 
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
