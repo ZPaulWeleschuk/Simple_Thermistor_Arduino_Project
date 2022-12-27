5a1c4a38577e376cd185ba23735460c8897bc867
Z. Paul Weleschuk <59177705+ZPaulWeleschuk@users.noreply.github.com>
2022-12-26 17:55:04 -0700

    bulk of coding done
    bulk of coding done. Buggy.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Simple_Thermistor_Arduino_Project
-Simple thermistor project with 7 segment display for Arduino Nano
+Arduino Nano project with NTC thermistor and 12 pin common cathode 7 segment display.
+
+//TODO: 
+parts list
+no library for display
+voltage divider
+self heating and using digital high for 5v source (note to self use multi meter and see if there is any voltage difference
+
--- simple_thermistor_project/simple_thermistor_project.ino
+++ simple_thermistor_project/simple_thermistor_project.ino
@@ -1,44 +1,376 @@
+using namespace std;
+
 /* LED segment positions
- *    ┌───┐
- *      A  
- * ┌ ┐└───┘┌ ┐
- * │ │     │ │
- * │F│     │B│
- * │ │     │ │
- * └ ┘┌───┐└ ┘
- *      G
- * ┌ ┐└───┘┌ ┐
- * │ │     │ │
- * │F│     │B│
- * │ │     │ │
- * └ ┘┌───┐└ ┘┌──┐
- *      D      DP
- *    └───┘   └──┘
+ *    ┌─────┐
+ *       A
+ * ┌ ┐└─────┘┌ ┐
+ * │ │       │ │
+ * │F│       │B│
+ * │ │       │ │
+ * └ ┘┌─────┐└ ┘
+ *       G
+ * ┌ ┐└─────┘┌ ┐
+ * │ │       │ │
+ * │E│       │C│
+ * │ │       │ │
+ * └ ┘┌─────┐└ ┘┌──┐
+ *       D       DP
+ *    └─────┘   └──┘
  */
 
-#define segA
-#define segB
-#define segC
-#define segD
-#define segE
-#define segF
-#define segG
-#define segDP
+#define segA 9
+#define segB 7
+#define segC 3
+#define segD 5
+#define segE 6
+#define segF 7
+#define segG 2
+#define segDP 4 // decimal point
 
-//note: digits start on left; 2031 == {dig1 = 2, dig2 = 0, dig3 =3, dig4 = 1}
-#define dig1
-#define dig2
-#define dig3
-#define dig4
-#define 
+// note: digits start on left; 2031 == {dig1 = 2, dig2 = 0, dig3 = 3, dig4 = 1}
+#define dig1 10
+#define dig2 12
+#define dig3 13
+#define dig4 11
+
+#define ntcInput A7
+#define ntcPower A5
+
+// thermistor variables
+
+int Vo;
+float R1 = 10000;
+float logR2, R2, T;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+int totalSamples = 5;
+float samples;
+int digits[4];
+
+char dTens;
+char dOnes;
+char dtenths;
+char dhundredths;
+
+String tempString;
+
+void setup()
+{
+  pinMode(segA, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+  pinMode(segDP, OUTPUT);
+  pinMode(dig1, OUTPUT);
+  pinMode(dig2, OUTPUT);
+  pinMode(dig3, OUTPUT);
+  pinMode(dig4, OUTPUT);
+
+  pinMode(ntcPower, OUTPUT);
+  pinMode(ntcInput, INPUT); // analog
+
+  Serial.begin(9600); // todo:
+}
+
+void loop()
+{
+  samples = 0.0;
+  // loop totalSample number of times
+  for(int i=0;i<totalSamples;i++)
+  {
+    // turn power on to ntc circuit / voltage divider
+     digitalWrite(ntcPower, HIGH);
+     delay(10);
+     Vo = analogRead(ntcInput);
+     //turn power off
+     digitalWrite(ntcPower, LOW);
+    R2 = R1 * (1023.0 / (float)Vo - 1.0);
+    logR2 = log(R2);
+    float tempC = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+    Serial.println(tempC -273.15);
+    samples += tempC - 273.15;
+    delay(90);
+  }
+
+  // get average of samples
+  float avgTemp = samples / totalSamples;
+  Serial.print("avgTemp: ");
+  Serial.println(avgTemp);
+
+  // cast float to string, convert back to digit, store in array, loop over array, display digits
+  tempString = String(avgTemp);
+  if (avgTemp < 100.0 && avgTemp >= 10.0)
+  {
+    dTens = tempString.charAt(0);
+    dOnes = tempString.charAt(1);
+    dtenths = tempString.charAt(3);
+    dhundredths = tempString.charAt(4);
+
+    //convert char to int
+    digits[0] = dTens - '0';
+    digits[1] = dOnes - '0';
+    digits[2] = dtenths - '0';
+    digits[3] = dhundredths -'0';
+
+    //round to one decimal point
+    //ex: 39.96 => 40.0(0)
+    if (digits[3] >=5){
+      digits[3] = 0;
+      digits[2]++;
+    }
+    if (digits[2] ==10){
+    digits[2] = 0;
+    digits[1]++;
+  }
+  if (digits[1] ==10){
+    digits[1] = 0;
+    digits[0]++;
+  }
+
+  }
+  else if (avgTemp < 10.0)
+  {
+      //dTens = tempString.charAt(0);
+    dOnes = tempString.charAt(0);
+    dtenths = tempString.charAt(2);
+    dhundredths = tempString.charAt(3);
+
+    //convert char to int
+    digits[0] = 0;
+    digits[1] = dOnes - '0';
+    digits[2] = dtenths - '0';
+    digits[3] = dhundredths -'0';
+  }
+
+      Serial.print("digits: ");
+    Serial.print(digits[0]);
+       Serial.print(", ");
+    Serial.print(digits[1]);
+        Serial.print(", ");
+    Serial.println(digits[2]);
+
+  //display
+  clearDisplay();           //Turn off all LED lights
+  pickDigit(1);          //Selection of a digital display
+  pickNumber(digits[0]);        //Display digital d1
+  delayMicroseconds(200);
+
+  clearDisplay();           //Turn off all LED lights
+  pickDigit(2);          //Select the first two digital display
+  //displayDecimalPoint(2);          //Decimal display
+  pickNumber(digits[1]);        //Display digital d2
+  delayMicroseconds(200);
+
+  clearDisplay();           //Turn off all LED lights
+  pickDigit(3);          //Select the first three digital display
+  pickNumber(digits[2]);        //Display digital d3
+  delayMicroseconds(200);
+  
+  //set dig 4 to 'c'
+  clearDisplay();
+  digitalWrite(dig4, LOW);
+
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+  digitalWrite(segDP, LOW);
+}
 
 
-void setup() {
-  // put your setup code here, to run once:
+void pickNumber(int x)   //Defined pickNumber (x), whose role is to display digital x
+{
+  switch(x)
+  {
+    case 1: 
+           one(); 
+           break;
+    case 2: 
+           two(); 
+           break;
+    case 3: 
+           three(); 
+           break;
+    case 4: 
+           four(); 
+           break;
+    case 5: 
+           five(); 
+           break;
+    case 6: 
+           six(); 
+           break;
+    case 7: 
+	        seven(); 
+	         break;
+    case 8: 
+	         eight(); 
+	   break;
+    case 9: 
+	   nine(); 
+	   break;
+    default: 
+           zero(); 
+           break;
+  }
+}
+
+void displayDecimalPoint(int x)  //Decimal point setting Open
+{
+  digitalWrite(segDP, HIGH);
+}
+
+void pickDigit(int x)  //Defined pickDigit (x), whose role is to open the port dx
+{
+  digitalWrite(dig1, HIGH);
+  digitalWrite(dig2, HIGH);
+  digitalWrite(dig3, HIGH);
+  digitalWrite(dig4, HIGH);
+  switch(x)
+  {
+    case 1: 
+           digitalWrite(dig1, LOW); 
+           break;
+    case 2: 
+           digitalWrite(dig2, LOW); 
+           break;
+    case 3: 
+           digitalWrite(dig3, LOW); 
+           break;
+    default: 
+           digitalWrite(dig4, LOW); 
+           break;
+  }
+}
+
+void clearDisplay() // Clear LED screen
+{
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+  digitalWrite(segDP, LOW);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+
+void zero()  //Define those figures 0 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, LOW);
 }
+ 
+void one()  //Define those figures 1 cathode pin switch
+{
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+ 
+void two()  //Define those figures 2 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, LOW);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+}
+ 
+void three()  //Define those figures 3 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, HIGH);
+}
+ 
+void four()  //Define those figures 4 cathode pin switch
+{
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+}
+ 
+void five()  //Define those figures 5 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+}
+ 
+void six()  //Define those figures 6 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, LOW);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+}
+ 
+void seven()  //Define those figures 7 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+ 
+void eight()  //Define those figures 8 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, HIGH);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+}
+ 
+void nine()  //Define those figures 9 cathode pin switch
+{
+  digitalWrite(segA, HIGH);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, HIGH);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, HIGH);
+  digitalWrite(segG, HIGH);
+}
+
+
